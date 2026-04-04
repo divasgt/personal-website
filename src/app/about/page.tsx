98a26bae25d7459eac98038dd3ab2ecd8cda90ce
Divas <divas.verma1956@gmail.com>
2026-04-05 03:49:46 +0530

    add: add placeholder About and Good Stuff pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-export default function GoodStuffPage() {
+export default function AboutPage() {
   return (
     <div className="min-h-screen flex flex-col items-center pt-20 mx-5">
       Under construction
