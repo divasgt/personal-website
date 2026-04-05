630c9b9fc5c7beaa5a68b08b20812aa339355d52
Divas <divas.verma1956@gmail.com>
2026-04-05 11:55:48 +0530

    style: Improve footer style on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,17 @@
 export default function Footer() {
   return (
-    <footer className="py-5 mt-10 text-center text-neutral-500 text-xs tracking-wide whitespace-pre-wrap">
-      <p>
-        Made with <span className="">{"<3"}</span> by Divas Verma.{"      "}Last
-        updated:{" "}
-        {new Date().toLocaleDateString("en-IN", {
+    <footer className="py-5 mt-10 text-center text-neutral-500 text-xs tracking-wide">
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-1 sm:gap-4">
+        <p className="whitespace-nowrap">Made by Divas Verma.</p>
+        <p className="whitespace-nowrap">
+          Last updated: <time dateTime="2026-04-05">5 April 2026</time>
+          {/* {new Date().toLocaleDateString("en-IN", {
           day: "numeric",
           month: "long",
           year: "numeric",
-        })}
-      </p>
+          })} */}
+        </p>
+      </div>
     </footer>
   );
 }
