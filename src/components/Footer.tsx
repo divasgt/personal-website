d108593a9cac6c3a6c10f379faed1f5f25236c1d
Divas <divas@local>
2026-06-27 22:42:32 +0530

    chore: update site last updated time

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="flex flex-col sm:flex-row justify-center items-center gap-1 sm:gap-4">
         <p className="whitespace-nowrap">Made by Divas Verma.</p>
         <p className="whitespace-nowrap">
-          Last updated: <time dateTime="2026-04-05">5 April 2026</time>
+          Last updated: <time dateTime="2026-06-27">27 Jun 2026</time>
           {/* {new Date().toLocaleDateString("en-IN", {
           day: "numeric",
           month: "long",
